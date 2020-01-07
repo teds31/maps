04a3176a74f025e0330e3d8b7aab5356d885331f
teds31 <tstanowski31@gmail.com>
2020-01-07 12:45:01 -0800

    add all files

---
+++ src/Company.ts
@@ -0,0 +1,29 @@
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+	companyName: string;
+	catchPhrase: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
+	// color: string = 'blue';
+
+	constructor() {
+		(this.companyName = faker.company.companyName()), (this.catchPhrase = faker.company.catchPhrase());
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude())
+		};
+	}
+
+	markerContent(): string {
+		return `
+		<div>
+		<h3>Company Name: ${this.companyName}</h3>
+		<h4>Catchphrase: ${this.catchPhrase}</h4>
+		</div>
+		`;
+	}
+}
